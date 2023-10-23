066e90c3f1f2cd102343f288ceaeeb5b7098a82e
Jefferson Botelho <jefferson.botelho@aluno.unip.br>
2023-10-22 21:24:36 -0300

    padrao de projeto strategy

---
+++ src/Test.java
@@ -0,0 +1,28 @@
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+    }
+
+}
